2d37dfe28d711baacc7441fe760a580a17080310
NicoFRI <nicofritsch@hotmail.fr>
2017-03-01 17:26:19 +0100

    ctrl c+

--- module1.py
+++ module1.py
@@ -4,22 +4,16 @@ from megapi import *
 import signal
 import sys
 
-if __name__ == '__main__':
-        bot = MegaPi()
-        bot.start('/dev/ttyUSB0')
-        bot.motorRun(M1,0);
-#        sleep(1);
-        while 1:
-                bot.motorRun(M1,200);
-                bot.motorRun(M2,-200);
-                bot.ultrasonicSensorRead(7,onRead);
-
-
 def onRead(v):
-        print "distance:"+str(v)+" cm";
-from megapi import *
-import signal
-import sys
+	print "distance:"+str(v)+" cm";
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+print('Press Ctrl+C')
+signal.pause()
+
 
 if __name__ == '__main__':
         bot = MegaPi()
@@ -30,16 +24,5 @@ if __name__ == '__main__':
                 bot.motorRun(M1,200);
                 bot.motorRun(M2,-200);
                 bot.ultrasonicSensorRead(7,onRead);
-
-
-def onRead(v):
-	print "distance:"+str(v)+" cm";
-
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-print('Press Ctrl+C')
-signal.pause()
 
 
