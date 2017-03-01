b116f0d364bcd284e35ca88d7274575bd55364d1
NicoFRI <nicofritsch@hotmail.fr>
2017-03-01 16:40:52 +0100

    vitesseup

--- module1.py
+++ module1.py
@@ -6,24 +6,9 @@ if __name__ == '__main__':
         bot.motorRun(M1,0);
         sleep(1);
         while 1:
-                sleep(1);
-                bot.motorRun(M1,50);
-                bot.motorRun(M2,-50);
+                bot.motorRun(M1,200);
+                bot.motorRun(M2,-200);
                 bot.ultrasonicSensorRead(7,onRead);
-                sleep(1);
-                bot.motorRun(M1,0);
-                bot.motorRun(M2,-0);
-                bot.ultrasonicSensorRead(7,onRead);
-                sleep(1);
-                bot.motorRun(M1,-50);
-                bot.motorRun(M2,50);
-                bot.ultrasonicSensorRead(7,onRead);
-                sleep(1);
-                bot.motorRun(M1,0);
-                bot.motorRun(M2,-0);
-                bot.ultrasonicSensorRead(7,onRead);
-
-		        
 
 
 def onRead(v):
