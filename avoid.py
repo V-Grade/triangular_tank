cc39e6d5e95d4a1527f7b7261fbbfb97ec6609d1
NicoFRI <nicofritsch@hotmail.fr>
2017-03-08 14:16:58 +0000

    coucou

---
+++ avoid.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from megapi import *
+import signal
+import sys
+
+
+distance = 0
+
+
+def onRead(v):
+    global distance
+    distance = v
+	#print "distance:"+str(v)+" cm";
+
+
+def running():
+    
+        print("Press CTRL-C to stop.")
+        while 1:
+            bot.ultrasonicSensorRead(7,onRead);
+            print distance
+            if distance > 40:
+                bot.motorRun(M1,200);
+                bot.motorRun(M2,-200);
+                bot.ultrasonicSensorRead(7,onRead);
+                print distance
+            
+            else:
+                bot.motorRun(M1,0);
+                bot.motorRun(M2,0);
+                bot.ultrasonicSensorRead(7,onRead);
+              	print distance
+
+           
+
+
+
+if __name__ == '__main__':
+    bot = MegaPi()
+    bot.start('/dev/ttyUSB0')
+    print 'initialisation'
+    sleep(1);
+    
+    try:
+        running()
+        
+    except KeyboardInterrupt:
+        print erreur
+    
+    bot.motorRun(M1,0);
+    bot.motorRun(M2,0); 
+    sys.exit(0)
