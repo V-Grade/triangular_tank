f2a3eebcf718639a61f9255f751f2c406462c5b1
NicoFRI <nicofritsch@hotmail.fr>
2017-03-08 10:42:34 +0100

    A noblewoman named Elias seeks a company of adventurers to steal a caravan of exotic goods from a rival. Moreover, the party encounters an old ally now working against them.

--- module1.py
+++ module1.py
@@ -4,40 +4,39 @@ from megapi import *
 import signal
 import sys
 
+
+distance = 0
+
+
 def onRead(v):
-    print ''
+    distance = v
 	#print "distance:"+str(v)+" cm";
 
 
 def running():
-    while 1:
-        try:    
-
+    
+        print("Press CTRL-C to stop.")
+        while 1:
             sleep(0.5)
             bot.motorRun(M1,200);
             bot.motorRun(M2,-200);
-            distance = bot.ultrasonicSensorRead(7);
+            bot.ultrasonicSensorRead(7,onRead);
             print distance
-            
 
-        except KeyboardInterrupt as erreur:
-            print 'ctrl+c'
-            break
-
-    bot.motorRun(M1,0);
-    bot.motorRun(M2,0);          
-    sys.exit(0)
-    return
 
 
 if __name__ == '__main__':
     bot = MegaPi()
     bot.start('/dev/ttyUSB0')
-    bot.motorRun(M1,0);
+    print 'initialisation'
     sleep(1);
-    
-    running()
-
+    try:
+        running()
+    except KeyboardInterrupt as erreur:
+        print 'ctrl+c'
+        bot.motorRun(M1,0);
+        bot.motorRun(M2,0); 
+        sys.exit(0)
 
 
 
