5b60583add0d678f8ec53036f3a24169f6c1f625
V-Grade <vincent.grade@gmail.com>
2017-03-09 11:22:28 +0100

    changement droite et gauche 90

--- Essais.py
+++ Essais.py
@@ -18,68 +18,22 @@ UtraSonicFrontal = 7
 UtraSonicLateral = 4
 
 #temps
-temps90deg = 0.750
-
-#distances
-distanceFront = 0
-distanceLateral = 0
-
-#Directions de 1 a 4 dans le sens des aiguilles d'une montre
-direction = 1 
-
-def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
-    bot.motorRun(MoteurAvant,0);
-    bot.motorRun(MoteurArriere,0);
-    sleep(0.05)
-    bot.exiting=True
-    quit()
-
+temps90deg = 0.200
 
 bot = MegaPi()
 bot.start('/dev/ttyUSB0')
-
-signal.signal(signal.SIGINT, signal_handler)
-#signal.pause()
-
-
-#lecture des capteurs ultrasonic
-def ReadUltra():
-    bot.ultrasonicSensorRead(UtraSonicFrontal,onReadFront);
-    bot.ultrasonicSensorRead(UtraSonicLateral,onReadLateral);
-
-#r?cup?ration de la valeur de distance front
-def onReadFront(v):
-    global distanceFront
-    distanceFront = v
-
-#r?cup?ration de la valeur de distance Lat?rale
-def onReadLateral(v):
-    global distanceLateral
-    distanceLateral = v
 
 #tourner de 90? a gauche
 def turnLeft():
     bot.motorRun(MoteurAvant,motorSpeed*coefRotation);
     bot.motorRun(MoteurArriere,motorSpeed*coefRotation);
     sleep(temps90deg)
-    changeDirection(-1)
 
 #tourner de 90? a droite
 def turnRight():
     bot.motorRun(MoteurAvant,-motorSpeed*coefRotation);
     bot.motorRun(MoteurArriere,-motorSpeed*coefRotation);
     sleep(temps90deg)
-    changeDirection(1)
-
-#avancer tout droit
-def avance(s=0.050):
-    if not obstacleFront() :
-        bot.motorRun(MoteurAvant,motorSpeed+coefCorection);
-        bot.motorRun(MoteurArriere,-(motorSpeed-coefCorection));
-        sleep(s)
-    else:
-        stop()
 
 #stop
 def stop():
@@ -87,49 +41,11 @@ def stop():
     bot.motorRun(MoteurArriere,-(0));
 
 
-
-#changement de direction
-def changeDirection(d):
-    global direction
-    direction = direction + d
-    if (direction > 4): direction = 1
-    if (direction < 1): direction = 4
-
-#d?tection d'obstacle frontale
-def obstacleFront():
-    return (distanceFront < 40) 
-
-#d?tection d'obstacle lat?rale
-def obstacleLateral():
-    return (distanceLateral < 40)
-
-
-#tourner dans la meilleur direction
-def Tourne():
-    # tourne a gauche si on est dans la direction Nord
-    if direction == 1 :
-        turnLeft()
-    # fait demis tour si on est dans la direction Ouest
-    elif direction == 4 :
-        turnRight()
-        turnRight()
-    #tourne a gauche si on est dans la direction Est
-    elif direction == 2 :
-        turnLeft()
-
-
 if __name__ == '__main__':
     
 
     print 'initialisation'
     sleep(1);
-    
-    print 'lecture des capteurs de distances'
-    ReadUltra()
-    print distanceFront
-    print distanceLateral
-  
-    sleep(2)
     
     print 'tourne a gauche'
     turnLeft()
@@ -140,21 +56,3 @@ if __name__ == '__main__':
     turnRight()
 
     sleep(2)
-
-    print 'avance'
-    avance()
-    stop()
-
-    sleep(1)    
-
-    print 'avance pdt 1seconde'
-    avance(1)
-    stop()
-
-
-    print 'wait for ctrl+C'
-    sleep(10)
-
-    print 'quit'
-    quit()
-
